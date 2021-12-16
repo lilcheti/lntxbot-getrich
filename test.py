b2ea8772980c414001b6f77b4f6ff1666d52835e
rimthekid <farbod@nigge.rs>
2021-12-17 01:20:23 +0400

    add more accounts by changin ACC_NUMBER in config

--- test.py
+++ test.py
@@ -2,12 +2,23 @@ from telethon.sync import TelegramClient, events
 import config
 api_id = config.API_ID
 api_hash = config.API_HASH
+coroutines = []
 
-with TelegramClient('name', api_id, api_hash) as client:
+import asyncio
 
+async def listen(client):
+   await client.connect()
+   await client.run_until_disconnected()
    @client.on(events.NewMessage(from_users="lntxbot"))
    async def handler(event):
       print(await event.get_sender())
       await event.click(1)
+   
 
-   client.run_until_disconnected()
+async def main():
+   for i in range(config.ACC_NUMBER):
+      coroutines.append(listen(TelegramClient('user'+str(i), api_id, api_hash)))
+   await asyncio.gather(
+        *coroutines
+    )
+asyncio.run(main())
